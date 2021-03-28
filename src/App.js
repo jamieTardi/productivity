7429141e6ed37349374c70e267b106b7ee215c4b
Jamie Tardi <jamie.tardi@gmail.com>
2021-03-28 10:31:32 +0100

    creating intial layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { TopNav } from './components/index';
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<TopNav />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/LayoutColumns.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const LayoutColumns = () => {
+	return <div></div>;
+};
+
+export default LayoutColumns;
